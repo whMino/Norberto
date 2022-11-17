5180afde269d4b954d19cc52092fef94df2a7313
Bubus <100220132+Bubuz123@users.noreply.github.com>
2022-11-17 14:56:47 +0100

    Aggiunto file reader

--- Progetto sito/js/scriptindex.js
+++ Progetto sito/js/scriptindex.js
@@ -1,12 +1,43 @@
+var reader = new XMLHttpRequest();
+
 function Accedi(){
-    
-    location.replace("./home.html");
-    // window.open('home.html');
-    // window.close;
+    if(document.getElementById("textuser").value != "" && document.getElementById("textpw").value != "")
+    {
+        var stringa = Leggi();
+        alert("Acceduto correttamente");
+        //location.replace("./home.html");
+    }
+    else
+    {
+        alert("Campo mancante");
+    }
 }
 
 function Registra(){
+    if(document.getElementById("textuser").value != "" && document.getElementById("textpw").value != "")
+    {
+        alert("Registrato correttamente");
+        location.replace("./home.html");
+    }
+    else
+    {
+        alert("Campo mancante");
+    }
+}
 
-
-    
+function Leggi()
+{
+    reader.open('get', '../file esterni/Utenti.txt', false); 
+    reader.onreadystatechange = function ()
+    {
+        if(reader.readyState === 4)
+        {
+            if(reader.status === 200 || reader.status == 0)
+            {
+                var allText = reader.responseText; //allText è il file su una stringa
+                return allText;
+            }
+        }
+    }
+    reader.send(null);
 }

7caa5b60ee05e12b543359de9739c8eec7d68720
Bubus <100220132+Bubuz123@users.noreply.github.com>
2022-11-23 12:48:10 +0100

    Aggiunta visualizzazione intera dei giornali
    Home: Aggiunti link corretti per i cataloghi
    Cataloghi: Aggiunto lo scheletro
    Visualizzazione giornale: Aggiunte tutte le funzioni, preparato per funzioni aggiuntive dei cataloghi

---
+++ Progetto sito/js/scriptgiornale.js
@@ -0,0 +1,34 @@
+var reader = new XMLHttpRequest();
+let stringa = "";
+
+function inizio()
+{
+    localStorage.setItem("Percor", "../file esterni/Sport/Italia/Giornali.txt"); //Per testare
+    localStorage.setItem("Riga", 1);
+
+    Leggi();
+    let riga = localStorage.getItem("Riga")-1;
+    let righe = stringa.split("\n");
+    let valori = righe[riga].split(";");
+    let percorso = valori[2]+"#toolbar=0";
+    document.getElementById('pdfframe').src = percorso;
+    document.getElementById('titolo').innerHTML = valori[0];
+}
+
+function Leggi()
+{
+    let percorsotxt = localStorage.getItem("Percor");
+    reader.open('get', percorsotxt, false); 
+    reader.onreadystatechange = function ()
+    {
+        if(reader.readyState === 4)
+        {
+            if(reader.status === 200 || reader.status == 0)
+            {
+                var allText = reader.responseText; //allText è il file su una stringa
+                stringa = allText;
+            }
+        }
+    }
+    reader.send(null);
+}
